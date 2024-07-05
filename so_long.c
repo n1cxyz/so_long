30edc29c026f4c0df9f594ba69a81a6d2cac9944
Dominic <owerwa@gmail.com>
2024-07-05 06:36:25 +0200

    added animations,map parsing and sprites

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: dominicasal <dominicasal@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 12:52:13 by dasal             #+#    #+#             */
-/*   Updated: 2024/07/03 01:29:27 by dominicasal      ###   ########.fr       */
+/*   Updated: 2024/07/05 06:31:34 by dominicasal      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,29 @@
 void	ft_create_map(t_data *data, char *file);
 void	ft_initialize_variables(t_data *data);
 //		parsing
-void	ft_parse_map(t_data *data);
+void	ft_parse_map(t_data *data, char *map_file);
 void	ft_find_player(t_data *data);
+int		ft_check_file_format(char *str);
+int		ft_check_walls(t_data *data);
 //		error
 void	ft_error_free_exit(char **map, char *message);
 void	ft_error_exit(char *message);
 void	ft_free_map(char **map);
+void	ft_error_free_map_exit(char **map, char *message);
+void	ft_error_free_all_exit(t_data *data,char **map, char *message);
+void	ft_destroy_images(t_data *data);
 //		rendering
 int		ft_render_map(t_data *data);
 void	ft_render_background(t_data *data);
 void	ft_render_sprite(t_data *data, int x, int y);
 void	ft_render_player(t_data *data);
+void	ft_render_animation(t_data *data, int x, int y);
 //		utils
 char	*ft_strappend(char *s1, char *s2);
 //		mlx
 void	ft_file_to_image(t_data *data);
+void	ft_player_to_image(t_data *data);
+void	ft_animation_to_image(t_data *data);
 int		ft_initialize_mlx(t_data *data);
 //		hooks
 void	ft_create_hooks(t_data *data);
@@ -42,7 +50,7 @@ int	main(int ac, char **av)
 	{
 		ft_initialize_variables(&data);
 		ft_create_map(&data, av[1]);
-		ft_parse_map(&data);
+		ft_parse_map(&data, av[1]);
 		ft_initialize_mlx(&data);
 		//ft_render_map(&data);
 		ft_create_hooks(&data);
@@ -50,8 +58,18 @@ int	main(int ac, char **av)
 		
 	}
 	else
-		printf("map file missing\n");
-
+		write(2, "Error\nmap file missing", 22);
+	/* TO DO:
+	https://reactive.so/post/42-a-comprehensive-guide-to-so_long
+	-check memory management
+	-close window with x 
+	-file to image error handling
+	-map parsing
+	-sprite transparency
+	-exit game if game won 
+	-movement count on screen 
+	-sprite animation
+	-enemy */
 	return (0);
 }
 
@@ -65,30 +83,30 @@ void	ft_create_hooks(t_data *data)
 int	ft_handle_keypress(int keysym, t_data *data)
 {
 	if (keysym == XK_Escape || keysym == 53)
-	{
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-		printf("ESC key pressed. Exiting program.\n");
-		exit (EXIT_FAILURE);
-	}
+		ft_error_free_all_exit(data,data->map, "ESC key pressed. Exiting program.\n");
 	if (keysym == XK_w || keysym == 13)
 	{
 		if ((data->map[data->player_y - 1][data->player_x] != '1') && ((data->map[data->player_y - 1][data->player_x] != 'E' || data->collectible_count == 0)))
 			data->player_y -= 1;
+		data->player_direction = 1;
 	}
     if (keysym == XK_a || keysym == 0)
 	{
 		if ((data->map[data->player_y][data->player_x - 1] != '1') && ((data->map[data->player_y][data->player_x - 1] != 'E' || data->collectible_count == 0)))
 			data->player_x -= 1;
+			data->player_direction = 2;
 	}
     if (keysym == XK_s || keysym == 1)
 	{
 		if ((data->map[data->player_y + 1][data->player_x] != '1') && ((data->map[data->player_y + 1][data->player_x] != 'E' || data->collectible_count == 0)))
 			data->player_y += 1;
+			data->player_direction = 3;
 	}
     if (keysym == XK_d || keysym == 2)
 	{
 		if ((data->map[data->player_y][data->player_x + 1] != '1') && ((data->map[data->player_y][data->player_x + 1] != 'E' || data->collectible_count == 0)))
 			data->player_x += 1;
+		data->player_direction = 4;
 	}
     data->moves++;
     printf("Moves: %d\n", data->moves);
@@ -108,52 +126,21 @@ int	ft_render_map(t_data *data)
 	return (0);
 }
 
-/* int	ft_render_player(t_data *data)
-{
-	int	x;
-	int	y;
-
-	y = 0;
-	data->map_y = 0;
-	while (y < data->win_height)
-	{
-		x = 0;
-		data->map_x = 0;
-		while (x < data->win_width)
-		{
-			if (data->map[y][x] == 'P')
-			{
-				ft_render_player_sprite(data);
-				return (0);
-			}
-			x++;
-			data->map_x += SPRITE_SIZE;
-		}
-		y++;
-		data->map_y += SPRITE_SIZE;		
-	}
-	return (1);
-} */
-
 void	ft_render_background(t_data *data)
 {
 	int	x;
 	int	y;
 
 	y = 0;
-	data->map_y = 0;
 	while (y < data->win_height)
 	{
 		x = 0;
-		data->map_x = 0;
 		while (x < data->win_width)
 		{
 			ft_render_sprite(data, x, y);
 			x++;
-			data->map_x ++;
 		}
 		y++;
-		data->map_y ++;		
 	}
 }
 
@@ -164,44 +151,68 @@ void	ft_render_sprite(t_data *data, int x, int y)
 	type = data->map[y][x];
 	if (type == '1')
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
-		data->wall_ptr, data->map_x * SPRITE_SIZE, data->map_y * SPRITE_SIZE);
+		data->wall_ptr, x * SPRITE_SIZE, y * SPRITE_SIZE);
 	else if (type == 'E' && data->collectible_count != 0)
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
-		data->closed_exit_ptr, data->map_x * SPRITE_SIZE, data->map_y * SPRITE_SIZE);
+		data->closed_exit_ptr, x * SPRITE_SIZE, y * SPRITE_SIZE);
 	else if (type == 'E' && data->collectible_count == 0)
-		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
-		data->open_exit_ptr, data->map_x * SPRITE_SIZE, data->map_y * SPRITE_SIZE);
-	/* if (type == 'P')
-		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
-	data->player_ptr, data->player_x, data->player_y); */
+		/* mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+		data->open_exit_ptr, x * SPRITE_SIZE, y * SPRITE_SIZE); */
+		ft_render_animation(data, x, y);
 	else
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
-	data->floor_ptr, data->map_x * SPRITE_SIZE, data->map_y * SPRITE_SIZE);
+	data->floor_ptr, x * SPRITE_SIZE, y * SPRITE_SIZE);
 	if (type == 'C')
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
-	data->collectible_ptr, data->map_x * SPRITE_SIZE, data->map_y * SPRITE_SIZE);
+	data->collectible_ptr, x * SPRITE_SIZE, y * SPRITE_SIZE);
+}
+
+void	ft_render_animation(t_data *data, int x, int y)
+{
+	if (data->animation_count == 1)
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+	data->open_exit_ptr, x * SPRITE_SIZE, y * SPRITE_SIZE);
+	if (data->animation_count == 20)
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+	data->open_2_exit_ptr, x * SPRITE_SIZE, y * SPRITE_SIZE);
+	if (data->animation_count == 40)
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+	data->open_3_exit_ptr, x * SPRITE_SIZE, y * SPRITE_SIZE); 
+	data->animation_count++;
+	if (data->animation_count == 60)
+		data->animation_count = 0;
 }
 
 void	ft_render_player(t_data *data)
 {
-	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
-	data->player_ptr, data->player_x * SPRITE_SIZE, data->player_y * SPRITE_SIZE - 6);
+	if (data->player_direction == 1)
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+	data->player_front_ptr, data->player_x * SPRITE_SIZE, data->player_y * SPRITE_SIZE - 6);
+	if (data->player_direction == 2)
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+	data->player_left_ptr, data->player_x * SPRITE_SIZE, data->player_y * SPRITE_SIZE - 6);
+	if (data->player_direction == 3)
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+	data->player_back_ptr, data->player_x * SPRITE_SIZE, data->player_y * SPRITE_SIZE - 6);
+	if (data->player_direction == 4)
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+	data->player_right_ptr, data->player_x * SPRITE_SIZE, data->player_y * SPRITE_SIZE - 6);
+	
 }
 
 int	ft_initialize_mlx(t_data *data)
 {
 	data->mlx_ptr = mlx_init();
 	if (data->mlx_ptr == NULL)
-		return (MLX_ERROR);
+		ft_error_free_map_exit(data->map, "Error\nfailed to initialize mlx");
 	data->win_ptr = mlx_new_window(data->mlx_ptr, \
 	data->win_width * SPRITE_SIZE, data->win_height * SPRITE_SIZE, "so_long");
 	if (data->win_ptr == NULL)
 	{
-		free(data->win_ptr);
-		return (MLX_ERROR);
+		free(data->mlx_ptr);
+		ft_error_free_map_exit(data->map, "Error\nfailed to create new window");
 	}
 	ft_file_to_image(data);
-	//mlx_loop(data->mlx_ptr);
 	return (0);
 }
 
@@ -211,10 +222,9 @@ void	ft_create_map(t_data *data, char *file)
 	char	*buffer;
 	char	*result;
 
-	data->map_file = NULL;
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
-		ft_error_exit("error opening file\n");
+		ft_error_exit("Error\nerror opening file\n");
 	while (1)
 	{
 		buffer = get_next_line(fd);
@@ -223,12 +233,14 @@ void	ft_create_map(t_data *data, char *file)
 		data->map_file = ft_strappend(data->map_file, buffer);
 		free (buffer);
 	}
+	if (buffer == NULL && data->map_file == NULL)
+		ft_error_exit("Error\nEmpty file\n");
 	close(fd);
 	data->map = ft_split(data->map_file, '\n');
 	if (!data->map)
 		{
 			free (data->map_file);
-			ft_error_free_exit(data->map, "failed to initialize map\n");
+			ft_error_free_map_exit(data->map, "failed to initialize map\n");
 		}
 	free (data->map_file);
 }
@@ -236,6 +248,9 @@ void	ft_initialize_variables(t_data *data)
 {
     data->moves = 0;
 	data->collectible_count = 0;
+	data->player_direction = 1;
+	data->map_file = NULL;
+	data->animation_count = 0;
 	
 }
 
@@ -245,9 +260,20 @@ void	ft_error_exit(char *message)
 	exit (EXIT_FAILURE);
 }
 
-void	ft_error_free_exit(char **map, char *message)
+void	ft_error_free_map_exit(char **map, char *message)
 {
 	ft_free_map(map);
+	write(2, message, ft_strlen(message));
+	exit (EXIT_FAILURE);
+}
+
+void	ft_error_free_all_exit(t_data *data,char **map, char *message)
+{
+	ft_destroy_images(data);
+	ft_free_map(map);
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	//mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
 	write(2, message, ft_strlen(message));
 	exit (EXIT_FAILURE);
 }
@@ -262,38 +288,96 @@ void	ft_free_map(char **map)
 		free(map[i]);
 		i++;
 	}
+	free (map);
 }
 
 void	ft_file_to_image(t_data *data)
 {
+	ft_player_to_image(data);
+	ft_animation_to_image(data);
 	data->floor_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
-	"./textures/grass.xpm", &data->img_width, &data->img_height);
+	FLOOR, &data->img_width, &data->img_height);
 	if (data->floor_ptr == NULL) {
        fprintf(stderr, "Error: Failed to load sprite image\n");
        exit (MLX_ERROR);
 	}
 	data->wall_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
-	"./textures/wall.xpm", &data->img_width, &data->img_height);
+	WALL, &data->img_width, &data->img_height);
 	if (data->wall_ptr == NULL) {
        fprintf(stderr, "Error: Failed to load sprite image\n");
        exit (MLX_ERROR);
 	}
-	data->player_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
-	"./textures/guy_front.xpm", &data->img_width, &data->img_height);
-	if (data->player_ptr == NULL) {
-       fprintf(stderr, "Error: Failed to load sprite image\n");
-       exit (MLX_ERROR);
-	}
 	data->collectible_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
-	"./textures/trans_char.xpm", &data->img_width, &data->img_height);
+	COLLECTIBLE, &data->img_width, &data->img_height);
 	data->open_exit_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
-	"./textures/port1.xpm", &data->img_width, &data->img_height);
+	EXIT_OPEN, &data->img_width, &data->img_height);
 	data->closed_exit_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
-	"./textures/closed.xpm", &data->img_width, &data->img_height);
+	EXIT_CLOSED, &data->img_width, &data->img_height);
 
 }
 
-void	ft_parse_map(t_data *data)
+void	ft_player_to_image(t_data *data)
+{
+	data->player_front_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
+	PLAYER_FRONT, &data->img_width, &data->img_height);
+	if (data->player_front_ptr == NULL)
+	{
+       fprintf(stderr, "Error: Failed to load sprite image\n");
+       exit (MLX_ERROR);
+	}
+	data->player_back_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
+	PLAYER_BACK, &data->img_width, &data->img_height);
+	if (data->player_back_ptr == NULL)
+	{
+       fprintf(stderr, "Error: Failed to load sprite image\n");
+       exit (MLX_ERROR);
+	}
+	data->player_left_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
+	PLAYER_LEFT, &data->img_width, &data->img_height);
+	if (data->player_left_ptr == NULL)
+	{
+       fprintf(stderr, "Error: Failed to load sprite image\n");
+       exit (MLX_ERROR);
+	}
+	data->player_right_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
+	PLAYER_RIGHT, &data->img_width, &data->img_height);
+	if (data->player_right_ptr == NULL)
+	{
+       fprintf(stderr, "Error: Failed to load sprite image\n");
+       exit (MLX_ERROR);
+	}
+}
+void	ft_animation_to_image(t_data *data)
+{
+	data->open_2_exit_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
+	EXIT_OPEN_2, &data->img_width, &data->img_height);
+	if (data->open_2_exit_ptr == NULL)
+	{
+       fprintf(stderr, "Error: Failed to load sprite image\n");
+       exit (MLX_ERROR);
+	}
+	data->open_3_exit_ptr = mlx_xpm_file_to_image(data->mlx_ptr, \
+	EXIT_OPEN_3, &data->img_width, &data->img_height);
+	if (data->open_3_exit_ptr == NULL)
+	{
+       fprintf(stderr, "Error: Failed to load sprite image\n");
+       exit (MLX_ERROR);
+	}
+}
+void	ft_destroy_images(t_data *data)
+{
+	mlx_destroy_image(data->mlx_ptr, data->player_front_ptr);
+	mlx_destroy_image(data->mlx_ptr, data->player_back_ptr);
+	mlx_destroy_image(data->mlx_ptr, data->player_left_ptr);
+	mlx_destroy_image(data->mlx_ptr, data->player_right_ptr);
+	mlx_destroy_image(data->mlx_ptr, data->floor_ptr);
+	mlx_destroy_image(data->mlx_ptr, data->wall_ptr);
+	mlx_destroy_image(data->mlx_ptr, data->collectible_ptr);
+	mlx_destroy_image(data->mlx_ptr, data->open_exit_ptr);
+	mlx_destroy_image(data->mlx_ptr, data->closed_exit_ptr);
+}
+
+void	ft_parse_map(t_data *data, char *map_file)
 {
 	int	i;
 
@@ -303,14 +387,73 @@ void	ft_parse_map(t_data *data)
 	while (data->map[i] != NULL)
 	{
 		if (ft_strlen(data->map[i]) != data->win_width)
-		{
-			printf("wrong line length");
-			exit (EXIT_FAILURE);
-		}
+			ft_error_free_map_exit(data->map, "Error\nWrong line length");
 		data->win_height++;
 		i++;
 	}
+	if (data->win_width > 40 || data->win_height > 25)
+		ft_error_free_map_exit(data->map, "Error\nMap too large\n");
 	ft_find_player(data);
+	if (!(ft_check_walls(data)))
+		ft_error_free_map_exit(data->map, "Error\nMap not enclosed in walls\n");
+	if (!(ft_check_file_format(map_file)))
+		ft_error_free_map_exit(data->map, "Error\nWrong file format\n");
+	if (data->win_height == data->win_width)
+		ft_error_free_map_exit(data->map, "Error\nMap must be rectangular\n");
+
+}
+
+int	ft_check_walls(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->win_width)
+	{
+		if (data->map[0][i] != '1')
+		{
+			printf("1");
+			return (0);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < data->win_width)
+	{
+		if (data->map[data->win_height - 1][i] != '1')
+		{
+			printf("2\n");
+			return (0);
+		}
+		i++;
+	}
+	i = 0;
+	while (data->map[i] != NULL)
+	{
+		if (data->map[i][0] != '1' || data->map[i][data->win_width - 1] != '1')
+		{
+			printf("3");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+int	ft_check_file_format(char *str)
+{
+	int	len;
+
+	len = ft_strlen(str) - 1;
+	if (str[len] != 'r')
+		return (0);
+	if (str[len - 1] != 'e')
+		return (0);
+	if (str[len - 2] != 'b')
+		return (0);
+	if (str[len - 3] != '.')
+		return (0);
+	return (1);
+	
 }
 
 void	ft_find_player(t_data *data)
--- so_long.h
+++ so_long.h
@@ -13,11 +13,17 @@
 # define MLX_ERROR 1
 # define SPRITE_SIZE 32
 
-# define FLOOR 0
-# define WALL 1
-# define COLLECTIBLE 2
-# define EXIT 3
-# define PLAYER 4
+# define FLOOR "./textures/grass.xpm"
+# define WALL "./textures/wall.xpm"
+# define COLLECTIBLE "./textures/trans_char.xpm"
+# define EXIT_OPEN "./textures/port1.xpm"
+# define EXIT_CLOSED "./textures/closed.xpm"
+# define PLAYER_FRONT "./textures/guy_front.xpm"
+# define PLAYER_BACK "./textures/guy_back.xpm"
+# define PLAYER_LEFT "./textures/guy_left.xpm"
+# define PLAYER_RIGHT "./textures/guy_right.xpm"
+# define EXIT_OPEN_2 "./textures/port2.xpm"
+# define EXIT_OPEN_3 "./textures/port3.xpm"
 # define UP 13
 # define LEFT 2
 # define DOWN 1
@@ -41,9 +47,14 @@ typedef struct s_data
 	void	*img_ptr;
 	void	*wall_ptr;
 	void	*floor_ptr;
-	void	*player_ptr;
+	void	*player_front_ptr;
+	void	*player_back_ptr;
+	void	*player_left_ptr;
+	void	*player_right_ptr;
 	void	*collectible_ptr;
 	void	*open_exit_ptr;
+	void	*open_2_exit_ptr;
+	void	*open_3_exit_ptr;
 	void	*closed_exit_ptr;
 	char	**map;
 	char	*map_file;
@@ -52,6 +63,7 @@ typedef struct s_data
 	int		map_y;
 	int		player_x;
 	int		player_y;
+	int		player_direction;
 	int		win_width;
 	int		win_height;
 	int		img_width;
@@ -61,4 +73,5 @@ typedef struct s_data
 	int		y;
 	const char *filename;
 	int		collectible_count;
+	int		animation_count;
 }	t_data;
